05773b633daf5ba74780eaf1ad3e7fb8466b05a2
mrmurtz <murtaza.s.abidi@gmail.com>
2016-10-01 21:45:03 +0100

    refactored q4

--- lib/questions.rb
+++ lib/questions.rb
@@ -25,7 +25,7 @@ end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
-  array.reject {|item| item == nil || item == false}
+  array.compact.delete_if {|item| item == false}
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
@@ -43,6 +43,12 @@ end
 # [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
 # make sure you don't have the same pairing twice,
 def every_possible_pairing_of_students(array)
+  pairs = []
+  array.each do |person|
+    puts person
+    pair = Array.new(pairs.push(person))
+
+  end
 end
 
 # discard the first 3 elements of an array,
--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -27,12 +27,12 @@ describe 'the Friday test :)' do
     expect(n).to eq ['a', 'b', 'c']
   end
 
-  fit 'reverse_every_element_in_array' do
+  it 'reverse_every_element_in_array' do
     n = reverse_every_element_in_array ['dog', 'monkey', 'elephant']
     expect(n).to eq ['god', 'yeknom', 'tnahpele']
   end
 
-  it 'every_possible_pairing_of_students' do
+  fit 'every_possible_pairing_of_students' do
     n = every_possible_pairing_of_students(['Bob', 'Dave', 'Clive']) || []
     sorted = n.map {|pair| pair.sort}.sort_by {|pair| [pair.first, pair.last] }
 
