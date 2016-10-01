ddda491e8cfeea6e3fc6c880a67bb95213b115f3
mrmurtz <murtaza.s.abidi@gmail.com>
2016-10-01 18:49:08 +0100

    q4 complete using reject method

--- lib/questions.rb
+++ lib/questions.rb
@@ -25,6 +25,7 @@ end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.reject {|item| item == nil || item == false}
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
